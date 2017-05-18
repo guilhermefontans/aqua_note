2ff9296d1ba547606573b09ef2a57dffa481f12f
guilhermefontans <guilherme.fontans@gmail.com>
2017-05-17 22:04:38 -0300

    Relacionamento tabelas.

---
+++ src/AppBundle/Entity/GenusNote.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fontans
+ * Date: 5/16/17
+ * Time: 10:31 PM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus_note")
+ */
+class GenusNote
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $userName;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $userAvatarFileName;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Genus")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $genus;
+
+    /**
+     * @return mixed
+     */
+    public function getGenus()
+    {
+        return $this->genus;
+    }
+
+    /**
+     * @param mixed $genus
+     * @return GenusNote
+     */
+    public function setGenus(Genus $genus)
+    {
+        $this->genus = $genus;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return GenusNote
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param mixed $userName
+     * @return GenusNote
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserAvatarFileName()
+    {
+        return $this->userAvatarFileName;
+    }
+
+    /**
+     * @param mixed $userAvatarFileName
+     * @return GenusNote
+     */
+    public function setUserAvatarFileName($userAvatarFileName)
+    {
+        $this->userAvatarFileName = $userAvatarFileName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * @param mixed $note
+     * @return GenusNote
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     * @return GenusNote
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+}
